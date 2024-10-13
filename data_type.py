2ade6008d2559eb814b22ab273b992a12901af15
devsrakib <devs.rakibulislam@gmail.com>
2024-10-13 12:48:54 +0600

    learned number data type

---
+++ data_type.py
@@ -0,0 +1,11 @@
+'''
+1, text => str
+2, numeric => int, float, complex
+3, sequence => list, tuple, range
+4, mapping => dict
+5, set => set, frozentype
+6, boolean => bool
+7, binary => bytes, bytearray, memoryview
+8, none => None
+'''
+
---
+++ numbers.py
@@ -0,0 +1,16 @@
+# int data type
+
+num1 = 420
+print(type(num1)) # checking type using type function
+print(num1) # normal print
+
+
+# floating data type
+num2 = 40.5
+print(type(num2)) # checking type using type function
+print(num2)# N
+
+#complex data type
+num3 = 456j # we must use j if we want to use complex data type
+print(num3)
+
